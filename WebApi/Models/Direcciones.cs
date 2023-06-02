7ceec50188d15ebb7d20834af34c6e0792fede76
ElBarbaro001 <casystem0@gmail.com>
2023-06-01 19:32:55 -0500

    Update Classes

--- WebApi/Models/Direcciones.cs
+++ WebApi/Models/Direcciones.cs
@@ -2,10 +2,10 @@
 {
     public class Direcciones
     {
-        
+        public string? dir_casa { get; set; }
         public string? nombres { get; set; }
         public string? primer_apellido { get; set; }
         public string? segundo_apellido { get; set; }
-        public string? dir_casa { get; set; }
+        
     }
 }
